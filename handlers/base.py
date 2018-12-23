f9427c7cd2917a1351c6a21b05c52ed515297237
Сметанин Никита <n.a.smetanin@gmail.com>
2018-12-23 15:31:04 +0500

    +html

--- handlers/base.py
+++ handlers/base.py
@@ -1,14 +1,15 @@
 from datetime import datetime
+import aiohttp_jinja2
 
 from aiohttp import web
 from aiohttp_session import get_session
 
 
 class Index(web.View):
-
+    @aiohttp_jinja2.template('index.html')
     async def get(self):
         conf = self.app['config']
-        return web.Response(text='Hello Aiohttp')
+        return dict(conf=conf)
 
 class Login(web.View):
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import base64
 import logging
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
 from cryptography import fernet
 from aiohttp_session import setup
@@ -16,6 +18,10 @@ def main():
     fernet_key = fernet.Fernet.generate_key()
     secret_key = base64.urlsafe_b64decode(fernet_key)
     setup(app, EncryptedCookieStorage(secret_key))
+
+    aiohttp_jinja2.setup(
+        app,
+        loader=jinja2.PackageLoader(package_name='main', package_path='templates'))
 
     setup_routes(app)
     app['config'] = BaseConfig
