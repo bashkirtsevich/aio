c2933b5abe122ef3e287f2b497104df40343de84
Сметанин Никита <n.a.smetanin@gmail.com>
2018-12-25 20:49:29 +0500

    взаимодействие бэка и фронта

--- handlers/base.py
+++ handlers/base.py
@@ -9,7 +9,23 @@ class Index(web.View):
     @aiohttp_jinja2.template('index.html')
     async def get(self):
         conf = self.app['config']
-        return dict(conf=conf)
+        session = await get_session(self)
+        user = {'I am var user'}
+        return dict(conf=conf, user=user)
+
+    @aiohttp_jinja2.template('index.html')
+    async def post(self):
+        data = await self.post()
+        msg=data['test']
+        if msg == 'asd':
+            msg = 'welcome'
+            user = {'I am var user'}
+            return dict(msg=msg, user=user)
+        else:
+            return dict(msg=msg)
+
+
+
 
 class Login(web.View):
     @aiohttp_jinja2.template('login.html')
@@ -17,19 +33,20 @@ class Login(web.View):
         session = await get_session(self)
         session['last_visit'] = str(datetime.utcnow())
         last_visit = session['last_visit']
-        text = 'Last visited {}'.format(last_visit)
-        return dict(text='{}'.format(text))
+        return dict()
 
     async def post(self):
         data = await self.post()
         login = data['login']
         password = data['password']
+        if login == 'admin' and password == 'admin':
+            export = {'yep'}
+            return dict(export=export)
 
         session = await get_session(self)
         session['user'] = {'login': login}
-
         location = self.app.router['index'].url_for()
-        return web.HTTPFound(location=location)
+        return web.HTTPFound(location=location, login=login)
 
 class Signup(web.View):
 
